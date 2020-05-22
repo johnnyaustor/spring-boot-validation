268b6cc1e364e6e0d7cbbcc2fcb8c1842fd71ccd
Johnny Austor <johnny.austor@gmail.com>
2020-05-22 08:41:32 +0700

    change validate

--- src/main/java/com/jap/springvalidation/service/validation/PeopleValidation.java
+++ src/main/java/com/jap/springvalidation/service/validation/PeopleValidation.java
@@ -1,6 +1,5 @@
 package com.jap.springvalidation.service.validation;
 
-import com.jap.springvalidation.dto.request.PeopleRequest;
 import com.jap.springvalidation.exception.ValidateException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
@@ -8,12 +7,12 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class PeopleValidation {
-    public void fullNameValidate(PeopleRequest peopleRequest) {
+    public void fullNameValidate(String fullName) {
         log.debug("PeopleValidation on fullNameValidate");
-        if ( peopleRequest.getFullName() == null ) throw new ValidateException("fullName not null");
+        if ( fullName == null ) throw new ValidateException("fullName not null");
     }
-    public void emailValidate(PeopleRequest peopleRequest) {
+    public void emailValidate(String email) {
         log.debug("PeopleValidation on emailValidate");
-        if ( peopleRequest.getEmail() == null ) throw new ValidateException("email not null");
+        if ( email == null ) throw new ValidateException("email not null");
     }
 }
